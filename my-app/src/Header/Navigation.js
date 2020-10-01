a66da0aac0aade2dee3f88c197785ca7ac36f4b5
JijoNair <jijonair@gmail.com>
2020-10-01 20:57:30 +0530

    First Draft

---
+++ my-app/src/Header/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+    return(
+
+        <div>
+            <nav>hello Deeksha ! I am callinbg from Navigation bar</nav>
+        </div>
+    );
+
+};
+
+export default Navigation;
